63ace4be2ab3ba8d08b4ac656e7b07179a06163f
minho65 <skt_ks@naver.com>
2020-07-21 19:12:02 +0900

    solved

--- minho/19238.cpp
+++ minho/19238.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -11,28 +12,62 @@ struct taxi {
     int y;
 };
 
+struct point {
+    int x;
+    int y;
+    int distance;
+};
+
 int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
 
-int use = 987654321;
-int idx = 0;
+int check(int y, int x, int p, int q, int loc){
+    queue<struct point> queue;
+    int use = 987654321;
 
-void check(int y, int x, int p, int q, int loc, int used){
-    if(y == p && x == q){
-        use = used;
-        idx = loc;
-        return;
-    }
-    if(used == N*N){
-        return;
-    }
+    int check[N][N];
+    for(int i =0; i < N; i++)
+        for(int j = 0; j < N; j++)
+            check[i][j] = 0;
 
-    for(int i = 0; i < 4; i++){
-        if(y + dy[i] < 0 || y + dy[i] >= N || x + dx[i] < 0 || x + dx[i] >=N){
-            continue;
+    check[y][x] = 1;
+    struct point tmp;
+    tmp.y = y;
+    tmp.x = x;
+    tmp.distance = 0;
+
+    queue.push(tmp);
+
+    while(!queue.empty()){
+        struct point node = queue.front();
+        queue.pop();
+
+        int curx = node.x;
+        int cury = node.y;
+        int curdis = node.distance;
+        
+        if(cury == p && curx == q){
+            if(use > curdis)
+                use = curdis;
         }
-        check(y + dy[i], x + dx[i], p,q, loc, used + 1);
+
+        for(int i = 0; i < 4; i++){
+            if(cury + dy[i] < 0 || cury + dy[i] >= N || curx + dx[i] < 0 || curx + dx[i] >=N)
+                continue;
+            if(check[cury+dy[i]][curx+dx[i]] == 0 && map[cury+dy[i]][curx+dx[i]] == 0){
+                check[cury+dy[i]][curx+dx[i]] = 1;
+                
+                struct point newnode;
+                newnode.x = curx + dx[i];
+                newnode.y = cury + dy[i];
+                newnode.distance = curdis + 1;
+
+                queue.push(newnode);
+            }
+        }
     }
+
+    return use;
 }
 
 int main()
@@ -79,21 +114,33 @@ int main()
             cout << fuel << "\n";
             return 0;
         }   
-        cout << total << "\n";
 
-        idx = 0;
-        use = 987654321;
+        int used = 987654321;
+        int temp = 0;
+        int idx = 0;
 
         // 출발지에서 손님까지
         for(int i = M-1; i >= 0; i--){
             if(person[i][0] == -1){ // skip case
                 continue;
             }
-            check(taxi.y, taxi.x, person[i][0],person[i][1], i, 0);
+            temp = check(taxi.y, taxi.x, person[i][0],person[i][1], i);
+            if(temp < used){
+                used = temp;
+                idx = i;
+            }
+            if(temp == used){
+                if(person[idx][0] > person[i][0]){
+                    idx = i;
+                }
+                else if(person[idx][0] == person[i][0] && person[idx][1] > person[i][1]){
+                    idx = i;
+                }
+            }
         }
 
-        if(use == N*N){
-            cout << "-1";
+        if(used == 987654321){
+            cout << "-1\n";
             return 0;
         }
         
@@ -102,28 +149,18 @@ int main()
         taxi.x = person[idx][1];
         person[idx][0] = -1;
         person[idx][1] = -1;
+        fuel -= used;
 
-        fuel -= use;
-
-        if(total < 0){
+        if(fuel < 0){
             cout << "-1\n";
             return 0;
         }
 
-        use = 987654321;
-        idx = 0;
-
         // 손님에서 목적지까지
-        for(int i = M-1; i >= 0; i--){
-            if(person[i][2] == -1){ // skip case
-                continue;
-            }
+        used = check(taxi.y, taxi.x, person[idx][2],person[idx][3], idx);
 
-            check(taxi.y, taxi.x, person[i][2],person[i][3], i, 0);
-        }
-
-        if(use == N*N){
-            cout << "-1";
+        if(used == 987654321){
+            cout << "-1\n";
             return 0;
         }
 
@@ -132,16 +169,14 @@ int main()
         taxi.x = person[idx][3];
         person[idx][2] = -1;
         person[idx][3] = -1;
-
-        fuel -= use;
+        fuel -= used;
 
         if(fuel < 0){
             cout << "-1\n";
             return 0;
         }
 
-        fuel += 2 * use;
-
+        fuel += 2 * used;
         total--;
     }
 
