5df3a6fd545af96616c0cb872ccd61e16fd78a15
minho65 <59965131+minho65@users.noreply.github.com>
2020-08-15 22:54:44 +0900

    서울지하철2호선 / 0815

--- minho/16947.cpp
+++ minho/16947.cpp
@@ -6,55 +6,56 @@
 
 using namespace std;
 
-int status[MAX];
+int N;
 bool visit[MAX];
-int N = 0;
+bool status[MAX];
+vector<int> graph[MAX];
 
-struct loc_info {
-    int loc;
+struct loc {
+    int cur;
     int length;
 };
 
-void dfs(vector<vector<int>> map, int cur, int before, int start){
-    if(status[start] >= 1)
+void dfs(int start, int cur, int cnt){
+    if(cur == start && cnt >= 2){
+        status[start] = true;
         return;
+    }
+
     visit[cur] = true;
 
-    // for문 사용법
-    for(int next : map[cur]){
-        if(next == start && before != start){
-            status[next] += 1;
-            return;
-        }
-        if(visit[next] == false){
-            dfs(map,next,cur,start);
-        }
-
-        if(status[start] == 1) return;
+    for(int next : graph[cur]){
+        if(visit[next] == false)
+            dfs(start,next,cnt+1);
+        else if(next == start && cnt >= 2)
+            dfs(start,next,cnt);
+        
+        if(status[start] == true) return;
     }
 }
 
-int bfs(vector<vector<int>> map, int cur){
-    int check[N] = {0,};
-    queue<loc_info> q;
+int bfs(int cur){
+    queue<loc> q;
+    int ans;
     q.push({cur,0});
-    int ans = 0;
-    check[cur] = 1;
+    memset(visit,false,sizeof(visit));
+    visit[cur] = true;
 
     while(!q.empty()){
-        loc_info point = q.front();
+        loc ncur = q.front();
         q.pop();
 
-        if(status[point.loc] == 1) {
-            ans = point.length;
+        if(status[ncur.cur] == true){
+            ans = ncur.length;
             break;
         }
 
-        for(int next : map[point.loc]){
-            if(check[next] == 0){
-                check[next] = 1;
-                q.push({next,point.length+1});
-            }
+        for(int next : graph[ncur.cur]){
+            if(visit[next] == true)
+                continue;
+            
+            visit[next] = true;
+            q.push({next,ncur.length+1});
         }
     }
 
@@ -64,34 +65,28 @@ int bfs(vector<vector<int>> map, int cur){
 int main()
 {
     cin >> N;
-
-    vector<vector<int>> map(N);
+    int a,b;
 
     for(int i = 0; i < N; i++){
-        int start = 0,end = 0;
-        cin >> start >> end;
-        map[start-1].push_back(end-1);
-        map[end-1].push_back(start-1);
+        cin >> a >> b;
+        graph[a-1].push_back(b-1);
+        graph[b-1].push_back(a-1);
     }
 
-    // 순환선 구분
-    memset(status,0,sizeof(status));
-    
+    memset(status,false,sizeof(status));
+
     for(int i = 0; i < N; i++){
         memset(visit,false,sizeof(visit));
-        dfs(map,i,-1,i);
+        dfs(i,i,0);
     }
- 
-    // 순환선 사이의 거리
+
     for(int i = 0; i < N; i++){
-        if(status[i] == 1){
+        if(status[i] == true)
             cout << "0 ";
-        }
-        else{
-            cout << bfs(map,i) << " ";
-        }
+        else
+            cout << bfs(i) << " ";
     }
     cout << "\n";
-    
+
     return 0;
 }
