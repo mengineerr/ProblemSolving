6e40e0f7c5a2bce91f8f62ab93bb40097165c485
minho65 <59965131+minho65@users.noreply.github.com>
2020-09-11 21:21:35 +0900

    Heapq (우선순위 큐)

--- minho/ProgrammersKit/Heap1.py
+++ minho/ProgrammersKit/Heap1.py
@@ -1,2 +1,18 @@
-# 우선순위 큐 (https://www.daleseo.com/python-priority-queue/)
+import heapq    # heapq 매우 빠른 우선순위 큐
 
+def solution(scoville, K):
+    answer = 0
+    
+    heap = []
+    
+    for num in scoville:
+        heapq.heappush(heap,num) # push
+        
+    while heap[0] < K:
+        try:
+            heapq.heappush(heap, heapq.heappop(heap) + heapq.heappop(heap) * 2) # push & pop
+        except IndexError:  # IndexError 사용법
+            return -1
+        answer += 1
+            
+    return answer
