b15871e9341e3f5e57c8c463a1ac2d5ab2313752
minho65 <59965131+minho65@users.noreply.github.com>
2020-09-15 20:59:59 +0900

    각 자릿수마다 숫자를 선택하는 index 기준 설정

---
+++ minho/ProgrammersKit/Greedy2.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    int num_len = number.length() - k;
+    int start = 0;
+    int last = number.length() - num_len;
+    
+    for(int i = last; i < number.length(); i++){
+        char tmp = '0';
+        for(int j = start; j <= i; j++){
+            if(number[j] > tmp){
+                tmp = number[j];
+                start = j;
+            }
+        }
+        number[start] = '0';
+        answer += tmp;
+    }
+    
+    return answer;
+}
