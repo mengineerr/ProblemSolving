90c5cddf1c3ecb48f6b54cfa5ead4501cde5c382
kchw1103 <kchw1103@naver.com>
2020-09-21 15:31:34 +0900

    Update 전화번호 목록.cpp

--- 김창우/코딩테스트 고득점 Kit/해시/전화번호 목록.cpp
+++ 김창우/코딩테스트 고득점 Kit/해시/전화번호 목록.cpp
@@ -1,1 +1,30 @@
-dd
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+	bool answer = true;
+	sort(phone_book.begin(), phone_book.end());
+	for(int i = 0 ; i < phone_book.size() - 1 ; i++){
+		if(phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size())) return false;
+	}
+    return answer;
+}
+
+int main(){
+
+	vector<string> phone_books;
+
+	phone_books.push_back("12");
+	phone_books.push_back("123");
+	phone_books.push_back("1235");
+	phone_books.push_back("567");
+	phone_books.push_back("88");
+
+	cout << solution(phone_books);
+
+	return 0;
+}
