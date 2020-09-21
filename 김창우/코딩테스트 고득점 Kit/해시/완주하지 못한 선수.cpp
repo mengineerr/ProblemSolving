557b8e24439ecc787bbd43aae3e576903f48df73
kchw1103 <kchw1103@naver.com>
2020-09-21 14:53:47 +0900

    Update 완주하지 못한 선수.cpp

--- 김창우/코딩테스트 고득점 Kit/해시/완주하지 못한 선수.cpp
+++ 김창우/코딩테스트 고득점 Kit/해시/완주하지 못한 선수.cpp
@@ -1,1 +1,36 @@
-dd
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+
+    unordered_map<string, int> m;
+
+    for(string name : participant) m[name]++;
+    for(string name : completion) m[name]--;
+    for(auto pair : m){
+    	if(pair.second > 0){
+    		return pair.first;
+    	}
+    }
+}
+
+int main(){
+
+	vector<string> participants;
+	vector<string> completions;
+
+	participants.push_back("leo");
+	participants.push_back("kiki");
+	participants.push_back("eden");
+
+	completions.push_back("eden");
+	completions.push_back("kiki");
+
+	cout << solution(participants, completions);
+
+	return 0;
+}
