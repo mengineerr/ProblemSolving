a7de43a304abbd2a3cf88f69a9f81fcd4813808a
minho65 <59965131+minho65@users.noreply.github.com>
2020-10-22 11:59:13 +0900

    이분탐색 / helped

---
+++ minho/ProgrammersKit/1022_BinarySearch02.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int distance, vector<int> rocks, int n) {
+    int answer = 0;
+    rocks.push_back(distance);
+    sort(rocks.begin(),rocks.end());
+    
+    int left = 1;
+    int right = distance;
+    
+    while(left <= right){
+        int mid = (left + right) / 2;
+        int idx = 0;
+        int cnt = 0;
+        
+        for(int next : rocks){
+            if(mid > next - idx)
+                cnt++;
+            else
+                idx = next; // 연속된 돌의 거리가 mid보다 작을 수 있는 경우
+        }
+        
+        if(cnt > n)
+            right = mid - 1;
+        else{
+            left = mid + 1;
+            answer = max(answer, mid);
+        }
+    }
+    
+    return answer;
+}
